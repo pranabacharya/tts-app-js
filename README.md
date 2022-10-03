4f0d177ce01c32ae0bd7df08948f80457bb95e2e
Pranab Acharya <pacharya808@gmail.com>
2022-10-03 22:38:33 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TEXT TO SPEECH APP (TTS App)
+    ## How to use 
+        - Open the index.html file on your web-browser
+        - Enter your text inside the **text box**
+        - Click on the **Read Text** button
+    ## Technology used
+        1 HTML5
+        2 CSS flex, positioning
+        3 `JavaScript`
+        4 `JavaScript event listeners`
+        5 `setTimeout()`
---
+++ app.js
@@ -0,0 +1,30 @@
+const readTextBtn = document.getElementById('read');
+const textBox = document.getElementById('text-to-read');
+const wordCount = document.querySelector('.fly-top-right');
+readTextBtn.addEventListener('click', ()=> {
+    const text = textBox.value;
+    if(text === '') return;
+    else{
+        const message = new SpeechSynthesisUtterance();
+        message.text = text;
+        speechSynthesis.speak(message);
+        setTimeout(()=>{
+            textBox.value = ''
+            wordCount.textContent = '0'
+        }, 4000)
+    }
+});
+
+
+textBox.addEventListener('input', () => {
+    let counter = textBox.value.length;
+    wordCount.innerText = counter;
+    if(counter < 30){
+        wordCount.style.backgroundColor = 'green';
+    }else if(counter < 80){
+        wordCount.style.backgroundColor = 'yellow';
+        wordCount.style.color = 'black';
+    }else {
+        wordCount.style.backgroundColor = 'red';
+    }
+})
